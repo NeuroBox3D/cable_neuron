0a8e1478a9f908f0c63324728013db050a623c45
Pascal Gottmann <pascal.gottmann@gcsc.uni-frankfurt.de>
2015-07-09 13:15:27 +0000

    HH_Kabelnew: Added Natrium-Kalium Pump

---
+++ Convert/Debug/pump_converted_standard_UG.cpp
@@ -0,0 +1,240 @@
+#include "pump_converted_standard_UG.h"	
+#include "lib_grid/lg_base.h" 
+#include "lib_disc/spatial_disc/elem_disc/elem_disc_interface.h" 
+#include "lib_disc/function_spaces/grid_function.h" 
+#include "lib_disc/function_spaces/local_transfer_interface.h" 
+#include <cmath> 
+namespace ug { 
+namespace cable { 
+ 
+ 
+// adding function which always inits_attachments 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::vm_disc_available()  
+{  
+	init_attachments();  
+}  
+ 
+ 
+ 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getnai() 
+{ 
+return nai; 
+} 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getipumpmax() 
+{ 
+return ipumpmax; 
+} 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getkm() 
+{ 
+return km; 
+} 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getn() 
+{ 
+return n; 
+} 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getnainit() 
+{ 
+return nainit; 
+} 
+template<typename TDomain> 
+double pump_converted_standard_UG<TDomain>::getcelsius() 
+{ 
+return celsius; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setnai(double val) 
+{ 
+nai = val; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setipumpmax(double val) 
+{ 
+ipumpmax = val; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setkm(double val) 
+{ 
+km = val; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setn(double val) 
+{ 
+n = val; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setnainit(double val) 
+{ 
+nainit = val; 
+} 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::setcelsius(double val) 
+{ 
+celsius = val; 
+} 
+ // creating Method for attachments 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::init_attachments() 
+{ 
+SmartPtr<Grid> spGrid = m_pVMDisc->approx_space()->domain()->grid(); 
+} 
+ 
+ 
+ 
+template<typename TDomain> 
+std::vector<number> pump_converted_standard_UG<TDomain>::state_values(number x, number y, number z) 
+{ 
+	 //var for output 
+	 std::vector<number> GatingAccesors; 
+ 
+	 typedef ug::MathVector<TDomain::dim> position_type; 
+ 
+	 position_type coord; 
+ 
+	 if (coord.size()==1) 
+	 	 coord[0]=x; 
+	 if (coord.size()==2) 
+	 { 
+	 	 coord[0] = x;
+	 	 coord[1] = y;
+	 } 
+	 if (coord.size()==3) 
+	 { 
+	 	 coord[0] = x;
+	 	 coord[1] = y;
+	 	 coord[2] = z;
+	 } 
+	 //accesors 
+	 typedef Attachment<position_type> position_attachment_type; 
+	 typedef Grid::VertexAttachmentAccessor<position_attachment_type> position_accesor_type; 
+ 
+	 // Definitions for Iteration over all Elements 
+	 typedef typename DoFDistribution::traits<Vertex>::const_iterator itType; 
+	 SubsetGroup ssGrp; 
+	 try { ssGrp = SubsetGroup(m_pVMDisc->approx_space()->domain()->subset_handler(), this->m_vSubset);} 
+	 UG_CATCH_THROW("Subset group creation failed."); 
+ 
+	 itType iter; 
+	 number bestDistSq, distSq; 
+	 Vertex* bestVrt; 
+ 
+	 // Iterate only if there is one Gtting needed 
+	 return GatingAccesors; 
+} 
+ 
+//Setters for states_outputs 
+ // Init Method for using gatings 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::init(Vertex* vrt, const std::vector<number>& vrt_values) 
+{ 
+//get celsius and time
+// inits temperatur from kalvin to celsius and some other typical neuron values
+number m_T, m_R, m_F; 
+m_T = m_pVMDisc->temperature(); 
+m_R = m_pVMDisc->R; 
+m_F = m_pVMDisc->F; 
+ 
+ 
+number celsius = m_pVMDisc->temperature_celsius(); 
+number dt = m_pVMDisc->time(); 
+// make preparing vor getting values of every edge 
+number v = vrt_values[VMDisc<TDomain>::_v_]; 
+number na = vrt_values[VMDisc<TDomain>::_na_]; 
+number k = vrt_values[VMDisc<TDomain>::_k_]; 
+
+ 
+nai =  nainit}; 
+}  
+ 
+ 
+ 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::update_gating(number newTime, Vertex* vrt, const std::vector<number>& vrt_values) 
+{ 
+// inits temperatur from kalvin to celsius and some other typical neuron values
+number m_T, m_R, m_F; 
+m_T = m_pVMDisc->temperature(); 
+m_R = m_pVMDisc->R; 
+m_F = m_pVMDisc->F; 
+ 
+ 
+number celsius = m_pVMDisc->temperature_celsius(); 
+ number FARADAY = m_pVMDisc->F; 
+ number dt = newTime - m_pVMDisc->time(); 
+number v = vrt_values[VMDisc<TDomain>::_v_]; 
+number na = vrt_values[VMDisc<TDomain>::_na_]; 
+number k = vrt_values[VMDisc<TDomain>::_k_]; 
+
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+} 
+ 
+ 
+ 
+template<typename TDomain> 
+void pump_converted_standard_UG<TDomain>::ionic_current(Vertex* ver, const std::vector<number>& vrt_values, std::vector<number>& outCurrentValues) 
+{ 
+ 
+// inits temperatur from kalvin to celsius and some other typical neuron values
+number m_T, m_R, m_F; 
+m_T = m_pVMDisc->temperature(); 
+m_R = m_pVMDisc->R; 
+m_F = m_pVMDisc->F; 
+ 
+ 
+number na = vrt_values[m_pVMDisc->_na_]; 
+number k = vrt_values[m_pVMDisc->_k_]; 
+number v =  vrt_values[m_pVMDisc->_v_]; 
+ 
+ 
+number t = m_pVMDisc->time(); 
+ 
+ 
+const number helpV = 1e3*(m_pVMDisc->R*m_pVMDisc->temperature())/m_pVMDisc->F; 
+number ION = helpV*(log(m_pVMDisc->k  WRITE ik_out()/k  WRITE ik)); 
+ 
+ 
+number ina = 3.0*inapump; 
+number ik = -2.0*inapump; 
+
+ 
+ 
+outCurrentValues.push_back( ipumpmax*(1/(1 + pow(km/nai,n)))); 
+} 
+ 
+ 
+//////////////////////////////////////////////////////////////////////////////// 
+//	explicit template instantiations 
+//////////////////////////////////////////////////////////////////////////////// 
+#ifdef UG_DIM_1 
+template class pump_converted_standard_UG<Domain1d>; 
+#endif 
+ 
+ 
+#ifdef UG_DIM_2 
+template class pump_converted_standard_UG<Domain2d>; 
+#endif 
+ 
+ 
+#ifdef UG_DIM_3 
+template class pump_converted_standard_UG<Domain3d>; 
+#endif 
+ 
+ 
+} // namespace cable
+} // namespace ug
+
+
---
+++ Convert/Debug/pump_converted_standard_UG.h
@@ -0,0 +1,116 @@
+#ifndef pump_converted_standard_UG_H_
+#define pump_converted_standard_UG_H_
+#include "channel_interface.h" 
+#include "lib_grid/lg_base.h" 
+#include "lib_grid/grid/grid_base_objects.h" 
+
+#include "lib_disc/spatial_disc/elem_disc/elem_disc_interface.h" 
+#include "lib_disc/spatial_disc/disc_util/fv1_geom.h" 
+#include "lib_disc/spatial_disc/disc_util/hfv1_geom.h" 
+#include "lib_disc/spatial_disc/disc_util/geom_provider.h" 
+#include "lib_disc/function_spaces/grid_function.h" 
+#include "lib_disc/function_spaces/local_transfer_interface.h" 
+#include "lib_disc/common/local_algebra.h" 
+#include "lib_disc/function_spaces/grid_function.h" 
+#include "lib_disc/function_spaces/interpolate.h" 
+
+#include "bridge/bridge.h" 
+#include "bridge/util.h" 
+#include "bridge/util_domain_algebra_dependent.h" 
+#include "bridge/util_domain_dependent.h" 
+
+#include "common/util/smart_pointer.h" 
+#include "common/util/vector_util.h" 
+
+#include "../../VM_Disc.h" 
+ 
+#include <vector> 
+#include <stdio.h> 
+#include "bindings/lua/lua_user_data.h" 
+namespace ug {
+namespace cable {
+
+
+// forward declaration 
+template <typename TDomain> 
+class VMDisc; 
+ 
+template <typename TDomain> 
+class pump_converted_standard_UG
+    : public IChannel<TDomain> 
+{ 
+    public: 
+ using IChannel <TDomain>::m_pVMDisc; 
+ 
+
+ 
+ 
+
+
+
+/// @copydoc IChannel<TDomain>::IChannel(cont char*) 
+pump_converted_standard_UG(const char* functions, const char* subsets) 
+try : IChannel<TDomain>(functions, subsets), 
+nai ( 0), 
+        ipumpmax  ( 0.0036   *1e-05), 
+        km ( 10.0        *1), 
+        n(1.5*1), 
+        nainit ( 4  *1), 
+        celsius ( 35  *1){} 
+UG_CATCH_THROW("Error in pump_converted_standard_UG initializer list. "); 
+ 
+ 
+/// @copydoc IChannel<TDomain>::IChannel(const std::vector<std::string>&) 
+pump_converted_standard_UG(const std::vector<std::string>& functions, const std::vector<std::string>& subsets) 
+try : IChannel<TDomain>(functions, subsets), 
+nai ( 0), 
+        ipumpmax  ( 0.0036   *1e-05), 
+        km ( 10.0        *1), 
+        n(1.5*1), 
+        nainit ( 4  *1), 
+        celsius ( 35  *1){} 
+UG_CATCH_THROW("Error in pump_converted_standard_UG initializer list. "); 
+/// destructor 
+ 
+virtual ~pump_converted_standard_UG() {}; 
+/// create attachments and accessors 
+void init_attachments(); 
+// inherited from IChannel 
+ 
+virtual void init(Vertex* vrt, const std::vector<number>& vrt_values); 
+virtual void update_gating(number newtime, Vertex* vrt, const std::vector<number>& vrt_values); 
+virtual void ionic_current(Vertex* v, const std::vector<number>& vrt_values, std::vector<number>& outCurrentValues); 
+virtual void vm_disc_available(); 
+virtual std::vector<number> state_values(number x, number y, number z); 
+
+ 
+double getnai(); 
+double getipumpmax(); 
+double getkm(); 
+double getn(); 
+double getnainit(); 
+double getcelsius(); 
+void setnai(double val); 
+void setipumpmax(double val); 
+void setkm(double val); 
+void setn(double val); 
+void setnainit(double val); 
+void setcelsius(double val); 
+
+ 
+protected: 
+private: 
+ 
+number nai ; 
+number         ipumpmax  ; 
+number         km ; 
+number         n; 
+number         nainit ; 
+number         celsius ; 
+}; 
+ 
+} // namespace cable
+} // namespace ug
+
+
+#endif // pump_converted_standard_UG_H_
