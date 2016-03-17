259dc3d02db63aaab8f62833ada8f1235e51aee9
Lukas Reinhardt <lukas.reinhardt@vsi.cs.uni-frankfurt.de>
2016-03-17 03:14:20 +0100

    sources added

--- splittedsynapse_handler/sources.cmake
+++ splittedsynapse_handler/sources.cmake
@@ -3,4 +3,6 @@ set (SOURCES ${SOURCES}
 		splittedsynapse_handler/PreAlphaSynapse.cpp
 		splittedsynapse_handler/IPostSynapse.cpp
 		splittedsynapse_handler/PostAlphaSynapse.cpp
+		splittedsynapse_handler/PreExp2Synapse.cpp
+		splittedsynapse_handler/PostExp2Synapse.cpp
 	)
